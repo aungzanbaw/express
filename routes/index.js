5035c049da62b9f260e424d073614e89f65904c4
Aung Zan Baw <aungzanbaw@gmail.com>
2020-04-15 16:41:41 +0630

    Fetching all records in home page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require('mongodb');
 
 const title = process.env.TITLE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: title });
+  const URL = process.env.URL;
+  const dbName = 'Library';
+  (async function(){
+		let client;
+    try {
+			client = await MongoClient.connect(URL);
+			const db = client.db(dbName);
+			const reponse = await db.collection('books').find().toArray();
+			res.json(reponse);
+    } catch (error) {
+			console.log(error);
+    }
+  })();
 });
 
 module.exports = router;
